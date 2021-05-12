d9eaa597bfcf269c25c6878a4457a65c983c064d
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-05-13 01:26:09 +0300

    finished paulse check

--- main.js
+++ main.js
@@ -5,6 +5,52 @@ const port = 3000;
 
 const users = ["John", "Mark"];
 
+
+
+
+
+//1)
+const logUsers = (req, res, next) => {
+    const err = new Error("Internal err");
+    err.status = 404
+    if (users[0] === undefined) {
+        next(err);
+    } else { 
+        console.log(users);
+        next();
+    }
+}
+
+//2)
+app.use((req, res, next) => {
+    console.log(users);
+    next();
+});
+
+
+// 3)
+const logMethod = (req, res, next) => {
+    console.log(req.method);
+
+    next();
+}
+app.use('/users', logMethod)
+
+
+//Pulse 4
+app.use(express.json())
+
+//5)
+app.use((err, req, res, next) => {
+    res.status = err.status
+    res.json({
+            error: {
+                status: err.status,
+                message: err.message
+            }
+        })
+})
+
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
